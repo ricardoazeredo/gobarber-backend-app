e33910e477a5d7fe3d7000475ef8ce1b97e64224
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2021-01-06 17:15:42 -0300

    Add Cors package from Api browser sercurity

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes';
 import uploadConfig from './config/upload';
@@ -10,12 +11,13 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
-  if(err instanceof AppError){
+  if (err instanceof AppError) {
     return response.status(err.statusCode).json({
       status: 'error',
       message: err.message,
@@ -26,7 +28,7 @@ app.use((err: Error, request: Request, response: Response, next: NextFunction) =
 
   return response.status(500).json({
     status: 'error',
-    message: 'Internal server error'
+    message: 'Internal server error',
   });
 });
 
